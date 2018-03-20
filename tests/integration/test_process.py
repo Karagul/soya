4bf97cc7e9a7237a4dc8ced8e9a992f683cb278e
lzd <lzd@liuzidongdeMacBook-Pro.local>
2018-03-20 16:34:56 +0800

    add integration test

---
+++ tests/integration/test_process.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from nose.tools import assert_equals
+from sqlalchemy import create_engine
+
+from soya import Soya
+
+
+class TestDatumImport(object):
+    """Integration test class for ``Soya._datum_import``
+    """
+    def setUp(self):
+        self.test_engine = create_engine(
+            'mysql+mysqldb://root:password@mysql:3306/soya_test?charset=utf8',
+            echo=False, encoding='utf8'
+        )
+        pd.DataFrame({'num1':[7, 8, 9]).to_sql(
+            name='table0'e, con=self.test_engine,
+            if_exists='replace', index=False
+        )
+
+    def test_soya_datum_import_no_chunk(self):
+        test_soya = Soya(
+            engine=self.test_engine,
+            input_dict={'table0', ['num1',]}
+        )
+
+        assert_equals(
+            test_soya._datum_import(),
+            {'table0': pd.DataFrame({'num1': [7, 8, 9]})}
+        )
+
+
